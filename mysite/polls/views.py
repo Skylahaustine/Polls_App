9bafc2326f12001a7dfe69a17ff08a2e7f582047
Austine <austineskylah9195@gmail.com>
2021-04-27 15:32:29 +0300

    Result

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.urls import reverse
+
 
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
